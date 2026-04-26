1ba6890558807df3fb18b1fee15309960b4f0a72
Pradip-Garai <pradipgarai173@gmail.com>
2026-04-26 18:49:06 +0530

    Add Markdown

--- app.py
+++ app.py
@@ -4,3 +4,51 @@ st.title("Hi!! I am Pradip Garai")
 st.header("Hello Guys")
 st.subheader("Hey Bro ")
 st.text("I am Software Engineer at TCS, I want to join GOOGLE")
+
+# markdown Heading
+st.markdown('# Hello')
+st.markdown('## Hello')
+st.markdown('### Hello')
+
+
+# Bold text
+st.markdown("**Hello** I am Pradip")
+
+# Italic 
+st.markdown("*I am Pradip Garai*")
+
+# code format
+str = "print('Hello World !!!')"
+st.code(str)
+
+# Horizontal line 
+st.markdown("---")
+
+# Link
+st.markdown('[GitHub](https://github.com/Pradip-Garai)')
+
+# table 
+table = '''
+| Syntax | Description |
+|--------- | ---------- |
+|Header | Title |
+|Paragraph | Text |
+'''
+
+st.markdown(table)
+
+
+
+# Json Feature
+json = {
+  "Name":"Pradip Garai",
+  "Age":20,
+  "Designation":"System Engineer"
+}
+
+st.json(json)
+
+
+# add imoge
+st.markdown(':joy:')
+st.markdown(':angry:')
