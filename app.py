fe6825cd91ac03158600f5fe49b47d75edd077d4
Pradip-Garai <pradipgarai173@gmail.com>
2026-04-26 18:29:56 +0530

    Hello

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.title("Hi!! I am Pradip Garai")
 st.header("Hello Guys")
 st.subheader("Hey Bro ")
-st.text("I am Software Engineer at TCS")
+st.text("I am Software Engineer at TCS, I want to join GOOGLE")
